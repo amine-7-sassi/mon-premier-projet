78578064054b5fb628e824b240cfec9910654d48
SASSI <amine.sassi@supcom.tn>
2026-06-03 14:02:46 +0100

    Ajout de la fonctionnalité division

--- script.py
+++ script.py
@@ -5,6 +5,12 @@ def calculatrice():
     
     print(f"Somme : {num1} + {num2} = {num1 + num2}")
     print(f"Produit : {num1} * {num2} = {num1 * num2}")
+    
+    # Nouvelle fonctionnalité ajoutée sur notre branche !
+    if num2 != 0:
+        print(f"Division : {num1} / {num2} = {num1 / num2}")
+    else:
+        print("Erreur : Division par zéro impossible !")
 
 if __name__ == "__main__":
     calculatrice()
